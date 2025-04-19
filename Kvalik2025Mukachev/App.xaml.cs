8bb28c2bf57173c32e3959bdac61be17cb0ab5fb
sunflour322 <myasnoypelmesh@gmail.com>
2025-04-19 10:48:18 +0300

    авторизация закончена

--- Kvalik2025Mukachev/App.xaml.cs
+++ Kvalik2025Mukachev/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Kvalik2025Mukachev.Database;
 
 namespace Kvalik2025Mukachev
 {
@@ -13,5 +14,16 @@ namespace Kvalik2025Mukachev
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static  Kvalik2025MukachevEntities db = new Kvalik2025MukachevEntities();
     }
 }
--- Kvalik2025Mukachev/Pages/AuthorizationPage.xaml.cs
+++ Kvalik2025Mukachev/Pages/AuthorizationPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Kvalik2025Mukachev.Database;
 
 namespace Kvalik2025Mukachev.Pages
 {
@@ -20,9 +21,60 @@ namespace Kvalik2025Mukachev.Pages
     /// </summary>
     public partial class AuthorizationPage : Page
     {
+        public static List<User> users { get; set; }
+        public static List<Master> masters { get; set; }
         public AuthorizationPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTb.Text.Trim();
+            string pass = PasswordTb.Text.Trim();
+
+            users = new List<User>(App.db.User.ToList());
+            User currentUser = users.FirstOrDefault(x => x.Login == login && x.Password == pass);
+            masters = new List<Master>(App.db.Master.ToList());
+            Master currentMaster = masters.FirstOrDefault(x => x.Login == login && x.Password == pass);
+            if(LoginTb.Text != "" && PasswordTb.Text != "")
+            {
+                if (currentMaster != null)
+                {
+                    NavigationService.Navigate(new RecordListPage());
+                    MessageBox.Show("Мастер");
+
+                }
+                else if (currentUser != null)
+                {
+                    if (currentUser.ID_role == 1)
+                    {
+                        MessageBox.Show("Клиент");
+
+                    }
+                    else if(currentUser.ID_role == 2)
+                    {
+                    MessageBox.Show("Администратор");
+
+                    }
+                    else if(currentUser.ID_role == 3)
+                    {
+                        MessageBox.Show("Модератор");
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Мы не нашли ваши данные в системе, попробуйте зайти снова");
+                    LoginTb.Text = "";
+                    PasswordTb.Text = "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Заполните все поля!!");
+
+            }
         }
     }
 }
